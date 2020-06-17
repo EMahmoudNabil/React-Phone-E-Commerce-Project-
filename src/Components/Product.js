c618014019d69e8f02f327c478903a29a62d5af8
EMahmoudNabil <e.mahmoudnabil@gmail.com>
2020-06-17 14:10:05 +0200

    function tester

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { ProductConsumer } from "../api/context";
+// import PropTypes from 'prop-types';
 
 export default  class Product extends Component {
     
@@ -44,6 +45,16 @@ export default  class Product extends Component {
         );
     }
 }
+
+// Product.PropTypes ={
+//     product:PropTypes.shape({
+//         id:PropTypes.number,
+//         img:PropTypes.string ,
+//         title:PropTypes.string ,
+//         price:PropTypes.number,
+//         inCart:PropTypes.bool
+//     }).isRequired
+// }
 
 
 const ProductWrapper = styled.div`
--- src/api/context.js
+++ src/api/context.js
@@ -17,12 +17,25 @@ class ProductProvider extends Component {
     addToCart = () => {
         console.log("Hello From add to Card")
     }
+    tester = () => {
+        console.log("State Products" , this.state.products[0].inCart)
+        console.log("Date Products" , storeProducts[0].inCart)
+        const tempProduct =[...this.state.products];
+        tempProduct[0].inCart =true
+        this.setState(()=>{
+            return{ products : tempProduct}
+        },()=>{
+        console.log("State Products" , this.state.products[0].inCart)
+        console.log("Date Products" , storeProducts[0].inCart)
+        })
+    }
     render() {
         return (
             <ProductContext.Provider value={{
                         ...this.state , 
                         handleDetail:this.handleDetail ,
                         addToCart:this.addToCart}}>
+            <button onClick={this.tester}> test me </button>
                 {this.props.children}
             </ProductContext.Provider>
         )
