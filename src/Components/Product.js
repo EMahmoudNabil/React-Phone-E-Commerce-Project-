45e8330e6f856bfe74a31b4f8c8bfc1d8812e437
EMahmoudNabil <e.mahmoudnabil@gmail.com>
2020-06-17 12:36:42 +0200

    product list Component

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -10,13 +10,36 @@ export default  class Product extends Component {
         return (
             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
             <div className="card">
-            <div  className="img-container p-5" onClick={console.log("you clicked  on my image container")}>
+            <div  className="img-container p-5" onClick={()=>console.log("you clicked  on my image container")}>
             <Link to="/details">
                 <img src={img} alt="product" className="card-img-top"/>
             </Link>
 
+            <button
+                    className="cart-btn"
+                    disabled={inCart ? true : false}
+                    onClick={() => {
+                    console.log("add to card")
+                    }}
+                >
+                    {inCart ? (
+                    <p className="text-capitalize mb-0" disabled>
+                        in cart
+                    </p>
+                    ) : (
+                    <i className="fa fa-cart-plus" />
+                    )}
+                </button>
             </div>
-            </div>
+            {/* Card Footer */}
+            <div className="card-footer d-flex justify-content-between">
+                <p className="align-self-center mb-0">{title}</p>
+                <h5 className="text-blue font-italic mb-0">
+                    <span className="mr-1">$</span>
+                    {price}
+                </h5>
+                </div>
+        </div>
             </ProductWrapper>
         );
     }
