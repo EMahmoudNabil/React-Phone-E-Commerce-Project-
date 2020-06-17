b33d5db9595c1a07f8488106aa016df43a41c268
EMahmoudNabil <e.mahmoudnabil@gmail.com>
2020-06-17 02:40:54 +0200

    inital setup for context

---
+++ src/api/context.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+const  ProductContext =React.createContext();
+//1- Provider
+// 2- Consumer
+class productProvider extends Component {
+    render() {
+        return (
+            <ProductContext.Provider value="Hello From Context">
+                {this.props.children}
+            </ProductContext.Provider>
+        )
+    }
+}
+
+
+const ProductConsumer = ProductContext.Consumer;
+
+export {productProvider ,ProductConsumer} ;
