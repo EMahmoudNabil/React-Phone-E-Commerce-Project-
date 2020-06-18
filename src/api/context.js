fcdb93f43aa9cace14cc8d81960ed474f67f91d0
EMahmoudNabil <e.mahmoudnabil@gmail.com>
2020-06-18 14:14:54 +0200

    add methode for cart

--- src/api/context.js
+++ src/api/context.js
@@ -9,6 +9,9 @@ class ProductProvider extends Component {
     state={
     products: [],
     detailProduct: detailProduct,
+    cart: [],
+    modalOpen: false,
+    modalProduct: detailProduct,
     }
 
     
@@ -38,17 +41,44 @@ class ProductProvider extends Component {
         });
     }
 
-    addToCart = () => {
-        console.log("Hello From add to Card")
-    }
+    addToCart = id => {
+        let tempProducts = [...this.state.products];
+        const index = tempProducts.indexOf(this.getItem(id));
+        const product = tempProducts[index];
+        product.inCart = true;
+        product.count = 1;
+        const price = product.price;
+        product.total = price;
+    
+        this.setState(() => {
+        return {
+            products: [...tempProducts],
+            cart: [...this.state.cart, product],
+            detailProduct: { ...product }
+        };
+        }, this.addTotals);
+    };
 
+    openModal = id => {
+        const product = this.getItem(id);
+        this.setState(() => {
+        return { modalProduct: product, modalOpen: true };
+    });
+    };
+    closeModal = () => {
+        this.setState(() => {
+        return { modalOpen: false };
+        });
+    };
 
     render() {
         return (
             <ProductContext.Provider value={{
                         ...this.state , 
                         handleDetail:this.handleDetail ,
-                        addToCart:this.addToCart}}>
+                        addToCart:this.addToCart ,  
+                        openModal: this.openModal,
+                        closeModal: this.closeModal,}}>
     
                 {this.props.children}
             </ProductContext.Provider>
