ba26c8034100f48165eee1579b3a14c364d6d48d
EMahmoudNabil <e.mahmoudnabil@gmail.com>
2020-06-19 00:14:33 +0200

    Card List & Card item Component

--- src/api/context.js
+++ src/api/context.js
@@ -9,7 +9,7 @@ class ProductProvider extends Component {
     state={
     products: [],
     detailProduct: detailProduct,
-    cart: [],
+    cart: [] ,
     modalOpen: false,
     modalProduct: detailProduct,
     cartSubTotal: 0,
