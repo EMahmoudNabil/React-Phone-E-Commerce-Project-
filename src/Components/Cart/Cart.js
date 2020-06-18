ef04d3e4c6308ea8fd7fc2927d5dd3ea0aa973d6
EMahmoudNabil <e.mahmoudnabil@gmail.com>
2020-06-18 22:29:46 +0200

    Empty Cart Component

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,14 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Title from '../Title'
 import CartColumns from './CartColumns'
+import EmptyCart from './EmptyCart'
+import { ProductConsumer } from "../../api/context";
+
 class Cart extends Component {
     render() {
         return (
             <section>
+            <ProductConsumer>
+            {value => {
+            const { cart } = value;
+            if (cart.length > 0) {
+            return (
+            <Fragment>
+                <Title name="your" title="cart" />
+                <CartColumns />
+            </Fragment>  
+            );}
+            else {
+            return <EmptyCart />;
+            }
+            }}
 
-            <Title name="your" title="cart" />
-            <CartColumns />
-
+            </ProductConsumer>
             </section>
         );
     }
