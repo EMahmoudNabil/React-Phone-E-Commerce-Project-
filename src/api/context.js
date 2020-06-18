93f2a802dd8af8f00ac5be450ebf29853fe43f61
EMahmoudNabil <e.mahmoudnabil@gmail.com>
2020-06-19 01:06:06 +0200

    Methde of Clear all & Remove

--- src/api/context.js
+++ src/api/context.js
@@ -82,10 +82,35 @@ class ProductProvider extends Component {
     console.log('this is decrement method')
     }
     removeItem = (id) => {
-        console.log('remove Item')
+        let tempProducts = [...this.state.products];
+        let tempCart = [...this.state.cart];
+    
+        const index = tempProducts.indexOf(this.getItem(id));
+        let removedProduct = tempProducts[index];
+        removedProduct.inCart = false;
+        removedProduct.count = 0;
+        removedProduct.total = 0;
+    
+        tempCart = tempCart.filter(item => {
+        return item.id !== id;
+        });
+    
+        this.setState(() => {
+        return {
+            cart: [...tempCart],
+            products: [...tempProducts]
+        };
+        }, this.addTotals);
         }
-    clearCart = (id) => {
-        console.log('clear Cart')
+    clearCart = () => {
+        this.setState(() => {
+            return { cart: [] };
+            },
+            () => {
+            this.setProducts();
+            this.addTotals();
+            }
+        );
         }
     addTotals = () => {
         let subTotal = 0;
